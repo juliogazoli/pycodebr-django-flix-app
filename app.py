36d2d3134f7b4471a4f1378dd970b2623e88f0ee
Julio Gazoli <julio_gazoli@hotmail.com>
2024-05-24 16:04:05 -0300

    menu options

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import streamlit as st
 
 
-st.title('Flix App')
-st.divider()
-st.text_input('Nome do filme')
-st.button('Salvar')
+def main():
+    st.title('Flix App')
+
+    menu_option = st.sidebar.selectbox(
+        'Selecione uma opção',
+        ['Início', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+    )
+
+    if menu_option == 'Início':
+        st.write('Início')
+    
+    if menu_option == 'Gêneros':
+        st.write('Lista de Gêneros')
+    
+    if menu_option == 'Atores/Atrizes':
+        st.write('Lista de Atores/Atrizes')
+    
+    if menu_option == 'Filmes':
+        st.write('Lista de Filmes')
+    
+    if menu_option == 'Avaliações':
+        st.write('Lista de Avaliações')
+
+
+if __name__ == '__main__':
+    main()
