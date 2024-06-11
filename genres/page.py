960d991e3c0dc4e037a5663d4b636053f26b7946
Julio Gazoli <julio_gazoli@hotmail.com>
2024-06-11 15:32:54 -0300

    connect genre page with api

--- genres/page.py
+++ genres/page.py
@@ -1,33 +1,31 @@
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
-
-genres = [
-    {
-        'id': 1,
-        'name': 'Ação'
-    },
-    {
-        'id': 2,
-        'name': 'Comédia'
-    },
-    {
-        'id': 3,
-        'name': 'Terror'
-    },
-]
+from genres.service import GenreService
 
 
 def show_genres():
-    st.write('Lista de Gêneros')
+    genre_service = GenreService()
+    genres = genre_service.get_genres()
 
-    AgGrid(
-        data=pd.DataFrame(genres),
-        reload_data=True,
-        key='genres_grid',
-    )
+    if genres:
+        st.write('Lista de Gêneros')
+        genres_df = pd.json_normalize(genres)
+        AgGrid(
+            data=pd.DataFrame(genres_df),
+            reload_data=True,
+            key='genres_grid',
+        )
+    else:
+        st.warning('Nenhum gênero encontrado.')
 
     st.title('Cadastrar novo Gênero')
     name = st.text_input('Nome do Gênero')
     if st.button('Cadastrar'):
-        st.success(f'Gênero "{name}" cadastrado com sucesso!')
+        new_genre = genre_service.create_genre(
+            name=name,
+        )
+        if new_genre:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar o gênero. Verifique os campos')
